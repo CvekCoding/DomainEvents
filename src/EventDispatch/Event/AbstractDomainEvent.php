5a4e52b3dc039110456b85ca4e4ea06c6dd67d91
Sergey Logachev <serg@baitek.org>
2020-03-01 17:59:09 +0300

    Added abstract event class

---
+++ src/EventDispatch/Event/AbstractDomainEvent.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * This file is part of the Aqua Delivery package.
+ *
+ * (c) Sergey Logachev <svlogachev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Cvek\DomainEventsBundle\EventDispatch\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+final class AbstractDomainEvent extends Event implements DomainEventInterface
+{
+    /**
+     * @var string
+     */
+    private string $lifecycleEvent;
+
+    /**
+     * @return string|null
+     */
+    public function getLifecycleEvent(): string
+    {
+        return $this->lifecycleEvent;
+    }
+
+    /**
+     * @param string $lifecycleEvent
+     */
+    public function setLifecycleEvent(string $lifecycleEvent): self
+    {
+        $this->lifecycleEvent = $lifecycleEvent;
+
+        return $this;
+    }
+}
